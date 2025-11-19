a446f0a9d51079ad7cfb14c2c8f9a151e11debd7
“aysstsn” <ayse.tosun2@std.yeditepe.edu.tr>
2025-11-19 14:45:31 +0300

    add today's lab

--- lab6.py
+++ lab6.py
@@ -1,7 +1,10 @@
 import numpy as np
 def arrays():
     array1 = np.array([-2.33, 100, 20, 33.2])
-    array2 = np.array([30, 12, 2, 70.2], [98.01, 4, 0, 7])
+    array2 = np.array([
+        [30, 12, 2, 70.2],
+        [98.01, 4, 0, 7]
+    ])
     array3 = np.arange(2, 12, 2)
     array4 = np.arange(20, -30, -10)
     array5 = np.linspace(0, 1, 4)
@@ -11,7 +14,11 @@ def arrays():
     array9 = np.diag(np.ones([2]), -1)
     array10 = np.array([np.zeros([4]), np.ones([4]), 2*np.ones([4])])
 
- 
+    return (
+        array1, array2, array3, array4, array5, 
+        array6, array7, array8, array9, array10
+    )
+
 def total_displacement(v1x, v1y, v2x, v2y, v3x, v3y):
     v1 = np.array([v1x, v1y]) #in km
     v2 = np.array([v2x, v2y]) #in km
